7b322c75bbee657ebbdcbb421d7da6c7484d7c5d
Alexander Dejeu <alexdejeu@gmail.com>
2017-02-20 23:34:17 -0800

    Added colors. At over 100 right now.

--- GradientColors/Gradients.swift
+++ GradientColors/Gradients.swift
@@ -379,16 +379,237 @@ class gradients{
   static let politeRumorsS3 = UIColor(red: 137.0/255.0, green: 137.0/255.0, blue: 186.0/255.0, alpha: 1)
   static let politeRumors : [UIColor] = [politeRumorsS1, politeRumorsS2, politeRumorsS3]
   
-  // 060 Polite Rumors
-  // TODO: MT to MM to MB
-  static let politeRumorsS1 = UIColor(red: 167.0/255.0, green: 166.0/255.0, blue: 203.0/255.0, alpha: 1)
-  static let politeRumorsS2 = UIColor(red: 137.0/255.0, green: 137.0/255.0, blue: 186.0/255.0, alpha: 1)
-  static let politeRumorsS3 = UIColor(red: 137.0/255.0, green: 137.0/255.0, blue: 186.0/255.0, alpha: 1)
-  static let politeRumors : [UIColor] = [politeRumorsS1, politeRumorsS2, politeRumorsS3]
-
-
-
+  // 061 Sweet Period Style
+  // TODO: SO MANY COLORS MT to MB ADD COLORS
+  static let sweetPeriodS1 = UIColor(red: 167.0/255.0, green: 166.0/255.0, blue: 203.0/255.0, alpha: 1)
+  static let sweetPeriodS2 = UIColor(red: 137.0/255.0, green: 137.0/255.0, blue: 186.0/255.0, alpha: 1)
+  static let sweetPeriodS3 = UIColor(red: 137.0/255.0, green: 137.0/255.0, blue: 186.0/255.0, alpha: 1)
+  static let sweetPeriodS4 = UIColor(red: 167.0/255.0, green: 166.0/255.0, blue: 203.0/255.0, alpha: 1)
+  static let sweetPeriodS5 = UIColor(red: 137.0/255.0, green: 137.0/255.0, blue: 186.0/255.0, alpha: 1)
+  static let sweetPeriodS6 = UIColor(red: 137.0/255.0, green: 137.0/255.0, blue: 186.0/255.0, alpha: 1)
+  static let sweetPeriodS7 = UIColor(red: 167.0/255.0, green: 166.0/255.0, blue: 203.0/255.0, alpha: 1)
+  static let sweetPeriodS8 = UIColor(red: 137.0/255.0, green: 137.0/255.0, blue: 186.0/255.0, alpha: 1)
+  static let sweetPeriodS9 = UIColor(red: 137.0/255.0, green: 137.0/255.0, blue: 186.0/255.0, alpha: 1)
+  static let sweetPeriod : [UIColor] = [politeRumorsS1, politeRumorsS2, politeRumorsS3]
   
+  // 062 Wide Matrix Style
+  // TODO: SO MANY COLORS MT to MB ADD COLORS
+  
+  // 063 Soft Cherish Style
+  // TODO: Ugh another huge one
+  
+  // 064 Red Salvation Style
+  // MT to MB
+  static let redSalvationS1 = UIColor(red: 69.0/255.0, green: 58.0/255.0, blue: 148.0/255.0, alpha: 1)
+  static let redSalvationS2 = UIColor(red: 244.0/255.0, green: 59.0/255.0, blue: 71.0/255.0, alpha: 1)
+  static let redSalvation : [UIColor] = [redSalvationS1, redSalvationS2]
+  
+  // 065 Burning Spring Style
+  // TODO : Radial thing S1 = Center and S2 equals the outer bound one
+  static let BurningSpringS1 = UIColor(red: 113.0/255.0, green: 221.0/255.0, blue: 166.0/255.0, alpha: 1)
+  static let BurningSpringS2 = UIColor(red: 112.0/255.0, green: 178.0/255.0, blue: 188.0/255.0, alpha: 1)
+  static let BurningSpring : [UIColor] = [BurningSpringS1, BurningSpringS2]
+
+  // 066 Night Party Style
+  // MT to MB
+  static let nightPartyS1 = UIColor(red: 212.0/255.0, green: 63.0/255.0, blue: 141.0/255.0, alpha: 1)
+  static let nightPartyS2 = UIColor(red: 2.0/255.0, green: 80.0/255.0, blue: 197.0/255.0, alpha: 1)
+  static let nightParty : [UIColor] = [nightPartyS1, nightPartyS2]
+  
+  // 067 Sky Glider Style
+  // MT to MB
+  static let skyGliderS1 = UIColor(red: 110.0/255.0, green: 69.0/255.0, blue: 226.0/255.0, alpha: 1)
+  static let skyGliderS2 = UIColor(red: 136.0/255.0, green: 221.0/255.0, blue: 206.0/255.0, alpha: 1)
+  static let skyGlider : [UIColor] = [skyGliderS1, skyGliderS2]
+  
+  // 068 Heaven Peach Style
+  // MT to MB
+  static let heavenPeachS1 = UIColor(red: 151.0/255.0, green: 217.0/255.0, blue: 225.0/255.0, alpha: 1)
+  static let heavenPeachS2 = UIColor(red: 217.0/255.0, green: 175.0/255.0, blue: 217.0/255.0, alpha: 1)
+  static let heavenPeach : [UIColor] = [heavenPeachS1, heavenPeachS2]
+  
+  // 070 Aqua Splash Style
+  // TODO: BL with Shift to TR with Shift
+  static let aquaSplashS1 = UIColor(red: 19.0/255.0, green: 84.0/255.0, blue: 122.0/255.0, alpha: 1)
+  static let aquaSplashS2 = UIColor(red: 128.0/255.0, green: 208.0/255.0, blue: 199.0/255.0, alpha: 1)
+  static let aquaSplash : [UIColor] = [aquaSplashS1, aquaSplashS2]
+  
+  // 071 Above Clouds Style
+  // TODO: The Strange Layering thing
+  
+  // 072 Spiky Naga Style
+  // TODO: ADD ALL THE COLORS!!
+  
+  // 073 Love Kiss Style
+  // MT to MB
+  static let loveKissS1 = UIColor(red: 255.0/255.0, green: 8.0/255.0, blue: 68.0/255.0, alpha: 1)
+  static let loveKissS2 = UIColor(red: 255.0/255.0, green: 177.0/255.0, blue: 153.0/255.0, alpha: 1)
+  static let loveKiss : [UIColor] = [loveKissS1, loveKissS2]
+
+  // 074 Sharp Glass Style
+  // TODO: The Strange Layering thing
+  
+  // 075 Clean Mirror Style
+  // BL to TR
+  static let cleanMirrorS1 = UIColor(red: 147.0/255.0, green: 165.0/255.0, blue: 207.0/255.0, alpha: 1)
+  static let cleanMirrorS2 = UIColor(red: 228.0/255.0, green: 239.0/255.0, blue: 233.0/255.0, alpha: 1)
+  static let cleanMirror : [UIColor] = [cleanMirrorS1, cleanMirrorS2]
+  
+  // 076 Premium Dark Style
+  // ML to MR
+  static let premiumDarkS1 = UIColor(red: 67.0/255.0, green: 67.0/255.0, blue: 67.0/255.0, alpha: 1)
+  static let premiumDarkS2 = UIColor(red: 0.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1)
+  static let premiumDark : [UIColor] = [premiumDarkS1, premiumDarkS2]
+  
+  // 077 Cold Evening Style
+  // MT to MB
+  static let coldEveningS1 = UIColor(red: 107.0/255.0, green: 140.0/255.0, blue: 206.0/255.0, alpha: 1)
+  static let coldEveningS2 = UIColor(red: 12.0/255.0, green: 52.0/255.0, blue: 131.0/255.0, alpha: 1)
+  static let coldEvening : [UIColor] = [coldEveningS1, coldEveningS2]
+  
+  // 078 Cochiti Lake Style
+  // BL to TR
+  static let cochitiLakeS1 = UIColor(red: 245.0/255.0, green: 200.0/255.0, blue: 245.0/255.0, alpha: 1)
+  static let cochitiLakeS2 = UIColor(red: 218.0/255.0, green: 221.0/255.0, blue: 250.0/255.0, alpha: 1)
+  static let cochitiLakeS3 = UIColor(red: 241.0/255.0, green: 244.0/255.0, blue: 249.0/255.0, alpha: 1)
+  static let cochitiLake : [UIColor] = [cochitiLakeS1, cochitiLakeS2, cochitiLakeS3]
+  
+  // 079 Summer Games Style
+  // ML to MR
+  static let summerGamesS1 = UIColor(red: 146.0/255.0, green: 254.0/255.0, blue: 157.0/255.0, alpha: 1)
+  static let summerGamesS2 = UIColor(red: 0.0/255.0, green: 201.0/255.0, blue: 255.0/255.0, alpha: 1)
+  static let summerGames : [UIColor] = [summerGamesS1, summerGamesS2]
+  
+  // 080 Passionate Bed Style
+  // ML to MR
+  static let passionateBedS1 = UIColor(red: 255.0/255.0, green: 117.0/255.0, blue: 140.0/255.0, alpha: 1)
+  static let passionateBedS2 = UIColor(red: 255.0/255.0, green: 126.0/255.0, blue: 179.0/255.0, alpha: 1)
+  static let passionateBed : [UIColor] = [passionateBedS1, passionateBedS2]
+
+  // 081 Mountain Rock Style
+  // ML to MR
+  static let mountainRockS1 = UIColor(red: 134.0/255.0, green: 143.0/255.0, blue: 150.0/255.0, alpha: 1)
+  static let mountainRockS2 = UIColor(red: 89.0/255.0, green: 97.0/255.0, blue: 100.0/255.0, alpha: 1)
+  static let mountainRock : [UIColor] = [mountainRockS1, mountainRockS2]
+  
+  // 082 Desert Hump Style
+  // ML to MR
+  static let desertHumpS1 = UIColor(red: 223.0/255.0, green: 165.0/255.0, blue: 121.0/255.0, alpha: 1)
+  static let desertHumpS2 = UIColor(red: 199.0/255.0, green: 144.0/255.0, blue: 129.0/255.0, alpha: 1)
+  static let desertHump : [UIColor] = [desertHumpS1, desertHumpS2]
+  
+  // 083 Jungle Day Style
+  // BL to TR
+  static let jungleDayS1 = UIColor(red: 139.0/255.0, green: 170.0/255.0, blue: 170.0/255.0, alpha: 1)
+  static let jungleDayS2 = UIColor(red: 174.0/255.0, green: 139.0/255.0, blue: 156.0/255.0, alpha: 1)
+  static let jungleDay : [UIColor] = [jungleDayS1, jungleDayS2]
+  
+  // 084 Phoenix Start Style
+  // ML to MR
+  static let phoenixStartS1 = UIColor(red: 248.0/255.0, green: 54.0/255.0, blue: 0.0/255.0, alpha: 1)
+  static let phoenixStartS2 = UIColor(red: 249.0/255.0, green: 212.0/255.0, blue: 35.0/255.0, alpha: 1)
+  static let phoenixStart : [UIColor] = [phoenixStartS1, phoenixStartS2]
+  
+  // 085 October Silenceiver Style
+  // TODO: TL Shifted to BR Shifted
+  static let octoberSilenceiverS1 = UIColor(red: 33.0/255.0, green: 212.0/255.0, blue: 253.0/255.0, alpha: 1)
+  static let octoberSilenceiverS2 = UIColor(red: 183.0/255.0, green: 33.0/255.0, blue: 255.0/255.0, alpha: 1)
+  static let octoberSilenceiver : [UIColor] = [octoberSilenceiverS1, octoberSilenceiverS2]
+  
+  // 086 Faraway River Style
+  // TODO: TL Shifted to BR Shifted
+  static let farawayRiverS1 = UIColor(red: 136.0/255.0, green: 211.0/255.0, blue: 206.0/255.0, alpha: 1)
+  static let farawayRiverS2 = UIColor(red: 110.0/255.0, green: 69.0/255.0, blue: 226.0/255.0, alpha: 1)
+  static let farawayRiver : [UIColor] = [farawayRiverS1, farawayRiverS2]
+  
+  // 087 Alchemist Lab Style
+  // TODO: TL Shifted to BR Shifted
+  static let alchemistLabS1 = UIColor(red: 36.0/255.0, green: 210.0/255.0, blue: 146.0/255.0, alpha: 1)
+  static let alchemistLabS2 = UIColor(red: 213.0/255.0, green: 88.0/255.0, blue: 200.0/255.0, alpha: 1)
+  static let alchemistLab : [UIColor] = [alchemistLabS1, alchemistLabS2]
+  
+  // 088 Over Sun Style
+  // TODO: ML Shifted to MR Shifted
+  static let overSunS1 = UIColor(red: 171.0/255.0, green: 236.0/255.0, blue: 214.0/255.0, alpha: 1)
+  static let overSunS2 = UIColor(red: 251.0/255.0, green: 237.0/255.0, blue: 150.0/255.0, alpha: 1)
+  static let overSun : [UIColor] = [overSunS1, overSunS2]
+  
+  // 089 Premium White Style
+  // TODO: MT to MB MANY COLORS
+  static let premiumWhiteS1 = UIColor(red: 233.0/255.0, green: 233.0/255.0, blue: 231.0/255.0, alpha: 1)
+  static let premiumWhiteS2 = UIColor(red: 239.0/255.0, green: 238.0/255.0, blue: 236.0/255.0, alpha: 1)
+  static let premiumWhiteS3 = UIColor(red: 238.0/255.0, green: 238.0/255.0, blue: 236.0/255.0, alpha: 1)
+  static let premiumWhiteS4 = UIColor(red: 213.0/255.0, green: 212.0/255.0, blue: 208.0/255.0, alpha: 1)
+  static let premiumWhite : [UIColor] = [premiumWhiteS1, premiumWhiteS2, premiumWhiteS3, premiumWhiteS4]
+  
+  // 090 Mars Party Style
+  // MT to MB
+  static let marsPartyS1 = UIColor(red: 155.0/255.0, green: 35.0/255.0, blue: 234.0/255.0, alpha: 1)
+  static let marsPartyS2 = UIColor(red: 95.0/255.0, green: 114.0/255.0, blue: 189.0/255.0, alpha: 1)
+  static let marsParty : [UIColor] = [marsPartyS1, marsPartyS2]
+  
+  // 091 Eternal Constance Style
+  // MT to MB
+  static let eternalConstanceS1 = UIColor(red: 83.0/255.0, green: 120.0/255.0, blue: 149.0/255.0, alpha: 1)
+  static let eternalConstanceS2 = UIColor(red: 9.0/255.0, green: 32.0/255.0, blue: 63.0/255.0, alpha: 1)
+  static let eternalConstance : [UIColor] = [eternalConstanceS1, eternalConstanceS2]
+
+  // 092 Japan Blush Style
+  // TODO: TL Shifted to BR Shifted
+  static let japanBlushS1 = UIColor(red: 250.0/255.0, green: 172.0/255.0, blue: 168.0/255.0, alpha: 1)
+  static let japanBlushS2 = UIColor(red: 221.0/255.0, green: 214.0/255.0, blue: 243.0/255.0, alpha: 1)
+  static let japanBlush : [UIColor] = [japanBlushS1, japanBlushS2]
+  
+  // 093 Smiling Rain Style
+  // TODO: TL Shifted to BR Shifted
+  static let smilingRainS1 = UIColor(red: 220.0/255.0, green: 176.0/255.0, blue: 237.0/255.0, alpha: 1)
+  static let smilingRainS2 = UIColor(red: 153.0/255.0, green: 153.0/255.0, blue: 204.0/255.0, alpha: 1)
+  static let smilingRain : [UIColor] = [smilingRainS1, smilingRainS2]
+  
+  // 094 Cloudy Apple Style
+  // MT to MB
+  static let cloudyAppleS1 = UIColor(red: 227.0/255.0, green: 238.0/255.0, blue: 255.0/255.0, alpha: 1)
+  static let cloudyAppleS2 = UIColor(red: 243.0/255.0, green: 231.0/255.0, blue: 233.0/255.0, alpha: 1)
+  static let cloudyApple : [UIColor] = [cloudyAppleS1, cloudyAppleS2]
+  
+  // 095 Big Mango Style
+  // MT to MB
+  static let bigMangoS1 = UIColor(red: 208.0/255.0, green: 150.0/255.0, blue: 147.0/255.0, alpha: 1)
+  static let bigMangoS2 = UIColor(red: 199.0/255.0, green: 29.0/255.0, blue: 111.0/255.0, alpha: 1)
+  static let bigMango : [UIColor] = [bigMangoS1, bigMangoS2]
+
+  // 096 Healthy Water Style
+  // TODO: ML Shifted to MR Shifted
+  static let healthyWaterS1 = UIColor(red: 150.0/255.0, green: 222.0/255.0, blue: 218.0/255.0, alpha: 1)
+  static let healthyWaterS2 = UIColor(red: 80.0/255.0, green: 201.0/255.0, blue: 195.0/255.0, alpha: 1)
+  static let healthyWater : [UIColor] = [healthyWaterS1, healthyWaterS2]
+  
+  // 097 Amour Amour Style
+  // MT to MB
+  static let amourAmourS1 = UIColor(red: 254.0/255.0, green: 81.0/255.0, blue: 150.0/255.0, alpha: 1)
+  static let amourAmourS2 = UIColor(red: 247.0/255.0, green: 112.0/255.0, blue: 98.0/255.0, alpha: 1)
+  static let amourAmour : [UIColor] = [amourAmourS1, amourAmourS2]
+  
+  // 098 Risky Concrete Style
+  // MT to MB
+  static let riskyConcreteS1 = UIColor(red: 235.0/255.0, green: 235.0/255.0, blue: 235.0/255.0, alpha: 1)
+  static let riskyConcreteS2 = UIColor(red: 220.0/255.0, green: 221.0/255.0, blue: 223.0/255.0, alpha: 1)
+  static let riskyConcreteS3 = UIColor(red: 196.0/255.0, green: 197.0/255.0, blue: 199.0/255.0, alpha: 1)
+  static let riskyConcrete : [UIColor] = [riskyConcreteS1, riskyConcreteS2, riskyConcreteS3]
+  
+  // 099 Strong Stick Style
+  // ML to MR
+  static let strongStickS1 = UIColor(red: 168.0/255.0, green: 202.0/255.0, blue: 186.0/255.0, alpha: 1)
+  static let strongStickS2 = UIColor(red: 93.0/255.0, green: 65.0/255.0, blue: 87.0/255.0, alpha: 1)
+  static let strongStick : [UIColor] = [strongStickS1, strongStickS2]
+  
+  // 100 Vicious Stance Style
+  // ML Shifted to MR Shifted
+  static let viciousStanceS1 = UIColor(red: 41.0/255.0, green: 50.0/255.0, blue: 60.0/255.0, alpha: 1)
+  static let viciousStanceS2 = UIColor(red: 72.0/255.0, green: 85.0/255.0, blue: 99.0/255.0, alpha: 1)
+  static let viciousStance : [UIColor] = [viciousStanceS1, viciousStanceS2]
+
+
   
   
 }
