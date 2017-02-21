ce5b6624747fe534c3a9318891ab7bbc90b5e36b
Alexander Dejeu <alexdejeu@gmail.com>
2017-02-21 13:19:32 -0800

    App icons and good good demo app started

--- GradientColors/ViewController.swift
+++ GradientColors/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var colorCollectionView: UICollectionView!
+  
+  //MARK : - View Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    print(colorCollectionView.frame)
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+}
+
+extension ViewController : UICollectionViewDelegate{
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    print(indexPath.item)
   }
+}
 
-
+extension ViewController : UICollectionViewDataSource{
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ColorCell", for: indexPath)
+    return cell
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 10
+  }
 }
 
